9f7199e0b471dad8023786d04ccef4849cf9b74e
bug-buster516 <80420347+bug-buster516@users.noreply.github.com>
2021-04-15 16:40:32 +0300

    Add files via upload

--- app/viewpage/viewpage.component.ts
+++ app/viewpage/viewpage.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import {AppComponent} from '../app.component'
+import {AuthService} from '../shared/services/auth.service'
+import {ActivatedRoute,Router} from '@angular/router'
 @Component({
   selector: 'app-viewpage',
   templateUrl: './viewpage.component.html',
   styleUrls: ['./viewpage.component.css']
 })
 export class ViewpageComponent implements OnInit {
-
-  constructor(private comp: AppComponent) { }
+	dataSymblos: any
+  constructor(private comp: AppComponent) { 
+  
+  }
 
   ngOnInit(): void {
 	  console.log(this.comp.tok)
