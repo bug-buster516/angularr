87f41a0f62732b1b935961ec202b703954012bca
bug-buster516 <80420347+bug-buster516@users.noreply.github.com>
2021-04-15 19:26:15 +0300

    Add files via upload

---
+++ app/shared/services/viewdata.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core'
+import {Response} from '../interface'
+import {HttpClient} from '@angular/common/http'
+import {Observable} from 'rxjs'
+import { map, retry, catchError } from 'rxjs/operators';
+@Injectable({
+	providedIn: 'root'
+})
+export class viewData {
+	constructor(private http: HttpClient){
+		
+	}
+	
+	getD (user: Response): Observable<any> {
+	return this.http.post('http://bmhmh.ho.ua/index.php/api/getRecords', user)
+	}
+}
